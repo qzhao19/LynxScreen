9d4fd8f881c7f5df7c36a59e448d6b216ec967c1
qzhao19 <qi.zhao@outlook.fr>
2025-12-25 23:47:14 +0800

    add cursor.types

---
+++ src/shared/types/cursor.types.ts
@@ -0,0 +1,9 @@
+// Status information of the remote cursor 
+// including position, color, identity, etc.
+export type RemoteCursorState = {
+  id: string
+  name: string
+  color: string
+  x: number
+  y: number
+}
