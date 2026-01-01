ab706926b75f0f6004e97cf6732547ebfce64ae6
qzhao19 <qi.zhao@outlook.fr>
2026-01-01 23:00:51 +0800

    add index.ts

---
+++ src/renderer/core/index.ts
@@ -0,0 +1,4 @@
+export { MediaStreamService } from "./webrtc/media-stream";
+export { DataChannelService } from "./webrtc/data-channel";
+export { WebRTCConnectionService } from "./webrtc/peer-connection";
+export { WebRTCService } from "./webrtc/webrtc-service";
