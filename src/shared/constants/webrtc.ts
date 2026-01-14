51aab8b0374732e2247d6abee814206f3e712a34
qzhao19 <qi.zhao@outlook.fr>
2026-01-14 23:39:20 +0800

    rename and update

--- src/shared/constants/webrtc.ts
+++ src/shared/constants/webrtc.ts
@@ -1,4 +1,5 @@
 import { IceServerConfig } from "../types/index";
+
 /**
  * Default WebRTC ICE server configurations.
  * Uses public STUN servers for basic NAT traversal.
@@ -9,3 +10,10 @@ export const DEFAULT_ICE_SERVERS: IceServerConfig[] = [
   { urls: "stun:stun1.l.google.com:19302" },
   { urls: "stun:stun2.l.google.com:19302" }
 ];
+
+
+/**
+ * URL-based signaling service for serverless P2P WebRTC connection.
+ * Encodes/decodes SDP offer and answer into shareable URLs.
+ */
+export const URL_PROTOCOL = "lynxscreen://";
