e6b46587f1a9c17eae418e00bd22e7225b8356e6
qzhao19 <qi.zhao@outlook.fr>
2026-01-17 22:38:44 +0800

    remove file

--- src/renderer/core/index.ts
+++
@@ -1,4 +0,0 @@
-export { MediaStreamService } from "./webrtc/media-stream";
-export { DataChannelService } from "./webrtc/data-channel";
-export { PeerConnectionService } from "./webrtc/peer-connection";
-export { WebRTCService } from "./webrtc/webrtc-service";
