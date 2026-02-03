1fa838f496d2c6479f0461f173927fcaf24e1243
qzhao19 <qi.zhao@outlook.fr>
2026-02-03 23:00:18 +0800

    add index

---
+++ src/renderer/components/ui/index.ts
@@ -0,0 +1,3 @@
+export { default as IconCircle } from "./IconCircle.svelte";
+export { default as Card } from "./Card.svelte";
+export { default as StatusIndicator } from "./StatusIndicator.svelte";
