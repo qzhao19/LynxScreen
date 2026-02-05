033737c17fdbc8b1f79aff3ea7ba4166a0ebf9a0
qzhao19 <qi.zhao@outlook.fr>
2026-02-05 21:18:33 +0800

    add index

---
+++ src/renderer/features/index.ts
@@ -0,0 +1,5 @@
+export { default as Home } from "./Home.svelte";
+export { default as ScreenWatcher } from "./ScreenWatcher.svelte";
+export { default as ScreenSharer } from "./ScreenSharer.svelte";
+export { default as ActiveSharing } from "./ActiveSharing.svelte";
+export { default as Settings } from "./Settings.svelte";
