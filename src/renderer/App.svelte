3b9b46e1e407c99b254fbdf43169d31f7ae5d954
qzhao19 <qi.zhao@outlook.fr>
2026-02-09 22:24:38 +0800

    add App.svelte

---
+++ src/renderer/App.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { currentPage } from "./stores/app";
+  import { Toast, HelpButton } from "./components/feedback";
+  import { Home, ScreenWatcher, ScreenSharer, ActiveSharing, Settings } from "./features";
+</script>
+
+<main>
+  {#if $currentPage === "home"}
+    <Home />
+  {:else if $currentPage === "watch"}
+    <ScreenWatcher />
+  {:else if $currentPage === "share"}
+    <ScreenSharer />
+  {:else if $currentPage === "active-sharing"}
+    <ActiveSharing />
+  {:else if $currentPage === "settings"}
+    <Settings />
+  {/if}
+
+  <!-- Global components -->
+  <Toast />
+  <HelpButton />
+</main>
+
+<style>
+  main {
+    min-height: 100vh;
+  }
+</style>
