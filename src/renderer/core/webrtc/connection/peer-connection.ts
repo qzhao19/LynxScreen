54fec7e3c706235bf281f73c83e0c8ebc2b6c23d
qzhao19 <qi.zhao@outlook.fr>
2026-03-19 23:11:21 +0800

    update

--- src/renderer/core/webrtc/connection/peer-connection.ts
+++ src/renderer/core/webrtc/connection/peer-connection.ts
@@ -1,4 +1,4 @@
-import log from "electron-log";
+import log from "electron-log/renderer";
 import { WebRTCConnectionConfig } from "../../../../shared/types/index";
 import { DataChannelService } from "../data/index";
 
