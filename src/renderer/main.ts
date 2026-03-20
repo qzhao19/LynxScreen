f159ce5b0242d74ded109289a81848f922ca1acd
qzhao19 <qi.zhao@outlook.fr>
2026-03-20 23:35:18 +0800

    update

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -1,6 +1,6 @@
 import { mount } from "svelte";
 import App from "./App.svelte";
-import "./styles/global.css";
+import "./frontend/styles/global.css";
 
 const app = mount(App, {
   target: document.body
