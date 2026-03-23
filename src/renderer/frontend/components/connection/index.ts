d37cdefc762d8d9b2fb1ac2696f1ba2a5a919119
qzhao19 <qi.zhao@outlook.fr>
2026-03-23 23:52:23 +0800

    update

--- src/renderer/frontend/components/connection/index.ts
+++ src/renderer/frontend/components/connection/index.ts
@@ -2,8 +2,8 @@
 export { default as ConnectionStatus } from "./ConnectionStatus.svelte";
 export { default as ConnectionUrl } from "./ConnectionUrl.svelte";
 export { default as MediaControls } from "./MediaControls.svelte";
-export { default as RemoteVideo } from "./RemoteVideo.svelte";
-export { default as RemoteCursors } from "./RemoteCursors.svelte";
+export { default as RemoteVideoInteract } from "./RemoteVideoInteract.svelte";
+export { default as RemoteCursorDisplay } from "./RemoteCursorDisplay.svelte";
 export { default as SessionTimer } from "./SessionTimer.svelte";
 
 // Re-export connection store for convenience
