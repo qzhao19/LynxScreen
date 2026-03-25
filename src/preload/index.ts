9526f1cfa49d54ccaff860e065da64d2bb15fbfc
qzhao19 <qi.zhao@outlook.fr>
2026-03-25 17:16:55 +0800

    update

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -2,7 +2,11 @@ import { contextBridge } from "electron";
 import { clipboardApi } from "./api/clipboard";
 import { loggerApi } from "./api/logger";
 
-contextBridge.exposeInMainWorld("electron", {
-  clipboard: clipboardApi,
-  logger: loggerApi
-});
+try {
+  contextBridge.exposeInMainWorld("electron", {
+    clipboard: clipboardApi,
+    logger: loggerApi
+  });
+} catch (error) {
+  console.error("[preload] Failed to expose API", error);
+}
