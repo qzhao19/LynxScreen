d09ed9fd3b2cfd40dae4c2e74ef9e30e4def0199
qzhao19 <qi.zhao@outlook.fr>
2026-03-31 00:03:44 +0800

    update

--- src/main/ipc/index.ts
+++ src/main/ipc/index.ts
@@ -1,7 +1,9 @@
 import { registerLoggerHandlers } from "./logger";
 import { registerClipboardHandlers } from "./clipboard";
+import { registerPowerHandlers } from "./power";
 
 export function registerIpcHandlers(): void {
   registerLoggerHandlers();
   registerClipboardHandlers();
+  registerPowerHandlers();
 }
