d18a90795d48191f8dde9810bea140926ccc6197
qzhao19 <qi.zhao@outlook.fr>
2026-03-31 00:03:57 +0800

    update

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,11 +1,13 @@
 import { contextBridge } from "electron";
 import { clipboardApi } from "./api/clipboard";
 import { loggerApi } from "./api/logger";
+import { powerApi } from "./api/power";
 
 try {
   contextBridge.exposeInMainWorld("electron", {
     clipboard: clipboardApi,
-    logger: loggerApi
+    logger: loggerApi,
+    power: powerApi
   });
 } catch (error) {
   console.error("[preload] Failed to expose API", error);
