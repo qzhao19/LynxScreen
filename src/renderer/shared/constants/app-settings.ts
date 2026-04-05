e6cad549a783d6d1a9bff253b0efb5d1c3b0bd09
qzhao19 <qi.zhao@outlook.fr>
2026-04-05 09:36:15 +0800

    move and rename

--- src/renderer/shared/constants/app-settings.ts
+++ src/renderer/shared/constants/app-settings.ts
@@ -3,7 +3,7 @@ import { AppSettings } from "../types/index";
 // Default app settings
 export const DEFAULT_APP_SETTINGS: AppSettings = {
   username: "User",
-  isMicrophoneEnabledOnConnect: false,
+  isMicrophoneEnabledOnConnect: true,
   iceServers: [
     { urls: "stun:stun.l.google.com:19302" },
     { urls: "stun:stun1.l.google.com:19302" }
